77cc40072f5747ffacd571e29e1ed7ab53409742
Abdalwanees <gabdo7682@gmail.com>
2024-08-30 01:03:18 +0300

    Add project files.

---
+++ App_Structure/Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App_Structure.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult GetMovies()
+        {
+            return View();
+        }
+        public string Index()
+        {
+            string text = $"Welcome People";
+            return text;
+        }
+    }
+}
---
+++ App_Structure/Startup.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_Structure
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Register to MVC Buildin Controller 
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+                //endpoints.MapGet("/hamada", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello hamada!");
+                //});
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern:"{controller=Movies}/{action=Index}/{Id?}"
+
+                    );
+            });
+        }
+    }
+}
